2dd13dbb0b64925398b1a98aa79118aea810eb99
Alex Santos <alexmansan@gmail.com>
2019-06-09 19:15:30 -0300

    Criação da classe categoria domain e alteração do CategoriaResource

--- src/main/java/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,30 @@
 package com.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+//	@RequestMapping(method=RequestMethod.GET)
+	@GetMapping
+	public List<Categoria> listar() {
+		System.out.println("Testando o Rest");
+		
+		Categoria cat1 = new Categoria((long) 1, "Informática");
+		Categoria cat2 = new Categoria((long) 2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+	
+		return lista;
 	}
 }
