7df17d3d6b402e89760c8bf2a1fd7ca8c6b730bc
Alex Santos <alexmansan@gmail.com>
2019-06-10 00:39:20 -0300

    Endpoint /clietes/{id} acrescentando as anotações de json nos mapeamentos de cliente x endereço e cidade x estados

--- src/main/java/com/cursomc/domain/Cliente.java
+++ src/main/java/com/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Cliente implements Serializable{
@@ -29,6 +30,7 @@ public class Cliente implements Serializable{
 	//modificado para integer
 	private Integer tipo;
 	
+	@JsonManagedReference // pode mostrar os endereços
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
---
+++ src/main/java/com/cursomc/services/ClienteService.java
@@ -0,0 +1,27 @@
+package com.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursomc.domain.Cliente;
+import com.cursomc.exceptions.ObjectNotFoundException;
+import com.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository dao;
+	
+	public Cliente buscar(Long id) {
+		// buscando por id e transformando em optional
+		Optional<Cliente> obj = dao.findById(id); 
+		
+		// retona a categoria ou uma exceção caso o id não exista 
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+
+	}
+}
