4894ba056bef18bc03f32643dfe8020ece4ec5da
Alex Santos <alexmansan@gmail.com>
2019-06-30 13:31:07 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,17 @@
+package com.cursomc.repositories; 
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cursomc.domain.ItemPedido;
+import com.cursomc.domain.ItemPedidoPK;
+
+/**
+ * 
+ * @author Alex
+ *
+ */
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, ItemPedidoPK>{
+
+}
